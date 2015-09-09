356984a4eb52d44f0c508f3b6f4424444944a0a2
Louise Ireland <louireland@gmail.com>
2015-09-09 23:29:29 +0100

    Update geocentrics.py

--- coorblimey/geocentrics.py
+++ coorblimey/geocentrics.py
@@ -78,6 +78,6 @@ class Geocentrics():
         #height, h
         h = p * (1 / math.cos(lat_rad)) - v
 
-        return [round(lon_deg, 8), round(lat_deg, 8), round(h, 3)]
+        return [round(lat_deg, 8), round(lon_deg, 8), round(h, 3)]
 
 
