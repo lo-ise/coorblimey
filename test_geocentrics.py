e59888e8c37acaec1bcc623271f8542806501c5c
Louise Ireland <louireland@gmail.com>
2015-09-09 23:38:43 +0100

    Switched back lat long ordering

--- test_geocentrics.py
+++ test_geocentrics.py
@@ -8,11 +8,11 @@ class TestGeocentrics(unittest.TestCase):
     def test_grs80_to_geographic(self):
         geo1      = Geocentrics(1418595, -689557, -6159338)
         lat_long1 = geo1.make_geographic()
-        self.assertEqual(lat_long1, [-25.92366687, -75.72836167, 30.705])
+        self.assertEqual(lat_long1, [-75.72836167, -25.92366687, 30.705])
 
         geo2      = Geocentrics(1425925.3230, -703655.7929, -6156081.8741)
         lat_long2 = geo2.make_geographic()
-        self.assertEqual(lat_long2, [-26.26509986, -75.61017435, 40.322])
+        self.assertEqual(lat_long2, [-75.61017435, -26.26509986, 40.322])
 
 
     def test_user_input(self):
@@ -26,7 +26,7 @@ class TestGeocentrics(unittest.TestCase):
     def test_wgs84_to_geographic(self):
         geo      = Geocentrics(1418595, -689557, -6159338, ellipsoid='WGS84')
         lat_long = geo.make_geographic()
-        self.assertEqual(lat_long, [-25.92366687, -75.72836167, 30.705])
+        self.assertEqual(lat_long, [-75.72836167, -25.92366687, 30.705])
 
 
 if __name__ == "__main__":
